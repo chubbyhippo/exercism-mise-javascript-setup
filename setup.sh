b7ab0fefe1a722dadb1e7ae75f53a51151b5c51a
chubbyhippo <mkmsacc@outlook.com>
2026-04-08 23:50:30 +0700

    Add command to download mise.toml via curl to setup.sh
    This ensures the necessary mise.toml file is automatically retrieved during setup. It improves the setup process by avoiding manual file downloads.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-
+curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-javascript-setup/refs/heads/main/mise.toml" -o "mise.toml"
 mise exec node@lts -- corepack enable pnpm
