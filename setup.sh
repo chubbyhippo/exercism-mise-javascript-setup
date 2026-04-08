9a0949ad4119f20688e6359a2135eeb2e10e2945
chubbyhippo <mkmsacc@outlook.com>
2026-04-08 23:51:08 +0700

    Add pnpm install command to setup script
    This ensures that PNPM dependencies are installed automatically when running the setup script. It simplifies the initial setup process for developers.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-javascript-setup/refs/heads/main/mise.toml" -o "mise.toml"
 mise exec node@lts -- corepack enable pnpm
+mise exec node@lts -- corepack pnpm install
