c4f73fadfa61be12d90bb1ab5d2de9964a22c73a
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-09 11:09:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 curl -k "https://raw.githubusercontent.com/chubbyhippo/exercism-mise-javascript-setup/refs/heads/main/mise.toml" -o "mise.toml"
 mise trust
+mise install
 mise exec node@lts -- corepack enable pnpm
 mise exec node@lts -- corepack pnpm install
